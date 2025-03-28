eae82901b98bb7b10750dc17469203b566fe07a1
MarwanElfiky <127157681+MarwanElfiky@users.noreply.github.com>
2025-03-28 02:08:59 +0200

    Almost rented

--- movie-store/Source.cpp
+++ movie-store/Source.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <fstream>
 #include <windows.h>
+#include <algorithm>
 using namespace std;
 
 struct customer {
 	int id;
+    bool isOverDue;
 	string name, rented_Movies[6];
 };
 
@@ -43,7 +45,9 @@ void display_MostRented();
 
 // helper functions
 date getCurrentDate();
-date validate_date(int day, int month, int year);
+date validate_DueDate(int day, int month, int year);
+int calculate_returnFees(int due_day, int due_month, int due_year, int index_ofMovie);
+int days_before_Duedate(int due_day, int due_month, int due_year);
 
 int main() {
 	main_menu();
@@ -134,41 +138,24 @@ void add_Customer(){
 
 void add_Movie() {
     cin.ignore();
-
+    int fees, overdue_fees;
+    string movie_name;
     cout << "Enter movie name" << endl;
-    getline(cin, movies[movies_count].name);
-
-    string choice;
-    cout << "Is the movie rented ?" << endl;
-    cin >> choice;
-    if (choice == "yes") {
-        movies[movies_count].is_Rented = TRUE;
-
-        cout << "Enter renting count" << endl;
-        cin >> movies[movies_count].renting_Count;
-
-        cout << "Enter due data\nday: " << endl;
-        cin >> movies[movies_count].due_Date.day;
-        cout << "Enter due data\nmonth: " << endl;
-        cin >> movies[movies_count].due_Date.month;
-        cout << "Enter due data\nyear: " << endl;
-        cin >> movies[movies_count].due_Date.year;
-
-        cout << "Enter returning data\nday: " << endl;
-        cin >> movies[movies_count].return_Date.day;
-        cout << "Enter returning data\nmonth: " << endl;
-        cin >> movies[movies_count].return_Date.month;
-        cout << "Enter returning data\nyear: " << endl;
-        cin >> movies[movies_count].return_Date.year;
-
-        cout << "Enter renting fee" << endl;
-        cin >> movies[movies_count].fees;
-
-        cout << "Enter overdue fees" << endl;
-        cin >> movies[movies_count].Overdue_Fees;
+    getline(cin, movie_name);
+    for (int i = 0; i <= movies_count; i++) {
+        if (movies[i].name == movie_name) {
+            cout << "Movie already exists" << endl;
+            main_menu();
+        }
     }
-    else
-        movies[movies_count].is_Rented = FALSE;
+    movies[movies_count].name = movie_name;
+    movies[movies_count].is_Rented = false;
+    cout << "Enter renting fees" << endl;
+    cin >> fees;
+    movies[movies_count].fees = fees;
+    cout << "Enter overdue fees (per day)" << endl;
+    cin >> overdue_fees;
+    movies[movies_count].Overdue_Fees = overdue_fees;
     movies_count++;
     main_menu();
 }
@@ -201,7 +188,7 @@ void list_allMovies() {
         Sleep(2000);
     }
     else {
-        cout << "No customers found" << endl;
+        cout << "No movies found" << endl;
         Sleep(2000);
     }
     main_menu();
@@ -214,6 +201,11 @@ void list_RentedMovies() {
                 cout << "Movie " << i + 1 << ":" << endl;
                 cout << "Name : " << movies[i].name << endl;
                 cout << "Rented times : " << movies[i].renting_Count << endl;
+            }
+            else {
+                cout << "No movies rented" << endl;
+                Sleep(2000);
+                break;
             }
         }
     }
@@ -256,7 +248,7 @@ void rent_Movies() {
                         for (int k = 0; k < 6; k++) { // loop over rented movies array in customer
 
                             if (customers[j].rented_Movies[k] == "") {
-                                customers[j].rented_Movies[k] = movie_name;
+                                customers[j].rented_Movies[k] = movie_name; // ---------------------- linked movie to the customer rented_Movies array ------------------------
                                 break;
                             }
 
@@ -276,11 +268,9 @@ void rent_Movies() {
                 movies[i].due_Date.day = today_date.day + 7;
                 movies[i].due_Date.month = today_date.month;
                 movies[i].due_Date.year = today_date.year;
-                movies[i].fees = 100;
-                movies[i].Overdue_Fees = 200;
                 movies[i].is_Rented = true;
                 movies[i].renting_Count++;
-                date validated_date = validate_date(movies[i].due_Date.day, movies[i].due_Date.month, movies[i].due_Date.year);
+                date validated_date = validate_DueDate(movies[i].due_Date.day, movies[i].due_Date.month, movies[i].due_Date.year);
                 cout << "Movie succesfully rented" << endl;
                 cout << "Due Date :" << validated_date.day << "-" << validated_date.month << "-" << validated_date.year << endl;
                 Sleep(2000);
@@ -298,16 +288,87 @@ void rent_Movies() {
 }
 
 void return_RentedMovies() {
+    cin.ignore();
+    cout << "Enter name of the movie to be returned" << endl;
+    string movie_name;
+    getline(cin, movie_name);
+    int total_fees = 0;
+    for (int i = 0; i <= movies_count; i++) { // search for the movie
 
+        if (movies[i].name == movie_name && movies[i].is_Rented == true) { // movie exists and is rented
+
+            for (int j = 0; j <= customer_count; j++) { // search for the customer rented the movie
+
+                for (int k = 0; k < 6; k++) { // search for the movie in the rented movies
+
+                    if (customers[j].rented_Movies[k] == movie_name) { // found the customer renting the movie now return it
+
+                        date return_date = getCurrentDate(); // returning today
+                        total_fees = calculate_returnFees(return_date.day, return_date.month, return_date.year, i);
+                        cout << "Total Charge = " << total_fees << endl;
+                        movies[i].is_Rented = false;
+                        customers[j].rented_Movies[k] = "";
+                        cout << "Movie returne ... Thanks for using our store" << endl;
+                        Sleep(2000);
+                        main_menu();
+                    }
+                }
+            }
+        }
+        else {
+            cout << "Movie Not Found" << endl;
+            Sleep(2000);
+            main_menu();
+        }
+    }
 }
 
-void overdue_Accounts() {
 
+void overdue_Accounts() {
+    date return_date = getCurrentDate();
+    for (int i = 0; i < movies_count; i++) {
+        for (int j = 0; j < customer_count; j++) {
+            for (int k = 0; k < 6; k++) {
+                if (movies[i].name == customers[j].rented_Movies[k]) {
+                    if (movies[i].is_Rented == true) {
+                        if (movies[i].due_Date.month < return_date.month) {
+                            customers[j].isOverDue = true;
+                        }
+                        if (movies[i].due_Date.month == return_date.month) {
+                            if (movies[i].due_Date.day < return_date.month) {
+                                customers[j].isOverDue = true;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    cout << "Overdue Accounts : " << endl;
+    for (int i = 0; i < customer_count; i++) {
+        if (customers[i].isOverDue == true) {
+            cout << "Customer " << i + 1 << " " << customers[i].name << endl;
+            cout << "id : " << customers[i].id << endl;
+        }
+    }
+    main_menu();
 }
 
 void display_MostRented() {
-
+    int rented_times = -1;
+    string most_rented_name;
+    for (int i = 0; i < movies_count; i++) {
+        if (movies[i].renting_Count > rented_times) {
+            rented_times = movies[i].renting_Count;
+            most_rented_name = movies[i].name;
+        }
+    }
+    cout << "Most Rented Movie : " << most_rented_name << endl;
+    cout << "Rented " << rented_times << "times" << endl;
 }
+
+// helpers
 
 date getCurrentDate() {
     time_t now = time(nullptr);
@@ -320,11 +381,10 @@ date getCurrentDate() {
     return { day, month, year };
 }
 
-date validate_date(int day, int month, int year) {
+date validate_DueDate(int day, int month, int year) { 
     for (int i = 0; i < 12; i++) {
-        if (month == (i + 1)) {
+        if (month == (i + 1) && month < 12) {
             if (day > months[i]) {
-                cout << i << endl;
                 cout << months[i] << endl;
                 int diffrence = day - months[i];
                 month++;
@@ -332,6 +392,71 @@ date validate_date(int day, int month, int year) {
                 break;
             }
         }
+        else { // december
+            if (day > months[i]) {
+                int diffrence = day - months[i];
+                month++;
+                month %= 12;
+                day = diffrence;
+                year++;
+            }
+        }
     }
     return { day, month, year };
+}
+
+int calculate_returnFees(int due_day, int due_month, int due_year, int index_ofMovie) { // takes due date ex:  2 - 4 - 2025   ----------   1 - 2 - 2025   ----------  29 - 3 - 2025 ----- 20 - 3 - 2025      
+    date return_date = getCurrentDate();
+    for (int i = 0; i < 12; i++) {
+        if (due_year > return_date.year) { // 2 down 9 to go
+            return movies[index_ofMovie].fees;
+        }
+        else if(due_year == return_date.year){
+
+            if (due_month > return_date.month) { // 2 down 7 to go
+                return movies[index_ofMovie].fees;
+            }
+
+            else if (due_month == return_date.month) { // month = month , year = year
+
+                if (due_day >= return_date.day) { // year == year, month == month, day >= day --------- 2 down 5 to go
+                    return movies[index_ofMovie].fees;
+                }
+
+                else { // due_day < return_date.day and month == month, year == year ------> start calculating overdue <------------ 1 down 4 to go
+                    int overdue_days = return_date.day - due_day;
+                    return movies[index_ofMovie].fees + (movies[index_ofMovie].Overdue_Fees * overdue_days);
+                }
+            }
+
+            else if (due_month < return_date.month) { // year == year, due_month < return_month -------> calculate overdue
+
+                if (due_day <= return_date.day) { // year == year, month < month, day <=> day ---- 1 down 2 to go (year < year)
+                    int overdue_days = days_before_Duedate(due_day, due_month, due_year);
+                    return movies[index_ofMovie].fees + (movies[index_ofMovie].Overdue_Fees * overdue_days);
+                }
+            }
+
+        }
+    }
+     return 0;
+}
+int days_before_Duedate(int due_day, int due_month, int due_year) {
+    date return_date = getCurrentDate();
+    int total_overDue = 0;
+    int days_before_month = 0;
+    if (due_year == return_date.year) {
+
+        if (due_month < return_date.month) {
+            int days_before_dueMonth_end = months[due_month - 1] - due_day;
+
+            for (int j = return_date.month; j > due_month; j--) {
+                days_before_month += months[j];
+            }
+            total_overDue = days_before_dueMonth_end + return_date.day + days_before_month;
+            return  total_overDue;
+        }
+        
+    }
+    return 0;
 }
